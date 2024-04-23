1640adf7b2e363f329e2817bdae3c49b70bc522d
Sun771 <marsntm@gmail.com>
2024-04-23 20:32:05 +0300

    Adding git

---
+++ app/js/script.js
@@ -0,0 +1,30 @@
+const btnMenu = document.querySelector("#btnMenu");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".contains-fade");
+const body = document.querySelector("body");
+
+btnMenu.addEventListener("click", function () {
+  console.log("clicked menu");
+
+  // open and close menu
+  if (header.classList.contains("open")) {
+    // closing
+    body.classList.remove("without-scroll");
+    header.classList.remove("open");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+  } else {
+    // opening
+    body.classList.add("without-scroll");
+    header.classList.add("open");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
+  }
+});
